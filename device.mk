f40bcc6f809db27eeaddd5f11d5a3fa996f08ec9
Krzysiek <kryzo123456@gmail.com>
2020-10-27 11:46:31 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Spectrum
 PRODUCT_PACKAGES += \
